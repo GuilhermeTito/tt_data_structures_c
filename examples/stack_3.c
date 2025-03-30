9f4a2ca1172c33ea8ec920a4aebe422ee5452f30
GuilhermeTito <guilhermeferrazronha123@gmail.com>
2025-03-30 11:22:22 -0300

    feat: stack resize example added

---
+++ examples/stack_3.c
@@ -0,0 +1,24 @@
+#include "stack.h"
+#include <stdio.h>
+
+int main(void)
+{
+    Stack stack;
+
+    stack_initialize(&stack, 2);
+
+    stack_push_str(&stack, "three");
+    stack_push_str(&stack, "two");
+
+    stack_resize(&stack, 3);
+
+    stack_push_str(&stack, "one");
+
+    printf("%s\n", stack_pop_str(&stack));
+    printf("%s\n", stack_pop_str(&stack));
+    printf("%s\n", stack_pop_str(&stack));
+
+    stack_finalize(&stack);
+    
+    return 0;
+}
