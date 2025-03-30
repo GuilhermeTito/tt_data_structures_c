83a061afa3057669045c73ce4e5c8c1f4313d9c4
GuilhermeTito <guilhermeferrazronha123@gmail.com>
2025-03-29 21:23:47 -0300

    refactor: "tests" changed to "examples"

--- examples/stack_1.c
+++ examples/stack_1.c
@@ -1,4 +1,4 @@
-#include "..\src\stack.h"
+#include "stack.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
--- examples/stack_2.c
+++ examples/stack_2.c
@@ -1,4 +1,4 @@
-#include "..\src\stack.h"
+#include "stack.h"
 #include <stdio.h>
 
 int main(int argc, char **argv)
