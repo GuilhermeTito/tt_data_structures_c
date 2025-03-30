3eea0c4c54c08c44f4eb5d7d6bffdd046902f2ea
GuilhermeTito <guilhermeferrazronha123@gmail.com>
2025-03-29 22:00:53 -0300

    feat: examples updated

--- examples/stack_2.c
+++ examples/stack_2.c
@@ -1,18 +1,29 @@
 #include "stack.h"
 #include <stdio.h>
 
-int main(int argc, char **argv)
+int main(void)
 {
     Stack stack_int;
     Stack stack_char;
+    Stack stack_float;
+    Stack stack_double;
+    
     stack_initialize(&stack_int, 10);
     stack_initialize(&stack_char, 10);
+    stack_initialize(&stack_float, 10);
+    stack_initialize(&stack_double, 10);
     
     stack_push_int(&stack_int, 3);
     stack_push_int(&stack_int, -23);
 
     stack_push_char(&stack_char, 'j');
     stack_push_char(&stack_char, 'k');
+
+    stack_push_float(&stack_float, 43.4f);
+    stack_push_float(&stack_float, 1.2f);
+
+    stack_push_double(&stack_double, 99.99);
+    stack_push_double(&stack_double, 17.2);
 
     printf("Stack (int):\n");
     printf("%d\n", stack_pop_int(&stack_int));
@@ -22,8 +33,18 @@ int main(int argc, char **argv)
     printf("%c\n", stack_pop_char(&stack_char));
     printf("%c\n", stack_pop_char(&stack_char));
 
+    printf("Stack (float):\n");
+    printf("%f\n", stack_pop_float(&stack_float));
+    printf("%f\n", stack_pop_float(&stack_float));
+
+    printf("Stack (double):\n");
+    printf("%f\n", stack_pop_double(&stack_double));
+    printf("%f\n", stack_pop_double(&stack_double));
+
     stack_finalize(&stack_int);
     stack_finalize(&stack_char);
+    stack_finalize(&stack_float);
+    stack_finalize(&stack_double);
     
     return 0;
 }
